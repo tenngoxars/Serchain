334c64e20e50d47208434ffeae8b1313f032a2ba
tenngo <tenngoxars@hotmail.com>
2025-09-04 17:09:00 +0800

    update

--- webapp/app.py
+++ webapp/app.py
@@ -90,5 +90,5 @@ def api_query():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
